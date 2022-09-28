f37eede5c4ef1b45de533088758586af1ddae353
Pushpendra Sharma <pushpendrasharmaa2015@gmail.com>
2022-09-28 17:27:05 +0530

    accepted incoming change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,26 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
-=======
      <h1> My name is khan</h1>
     </div>
->>>>>>> cf1153f61797ee93301211348773b3e72147e18c
   );
 }
 
